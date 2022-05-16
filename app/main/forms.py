dd7c3295cc5830b661e2fa375a06860a1cb98f76
blancc-page <blancc.666@gmail.com>
2022-05-16 16:51:54 +0300

    fix[requirements.txt]: solve flask dependency conflicts

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,26 +1,26 @@
 from flask_wtf import FlaskForm 
 from wtforms import SelectField,StringField,SubmitField,TextAreaField,PasswordField,BooleanField
-from wtforms.validators import Required,Email,EqualTo
+from wtforms.validators import InputRequired,Email,EqualTo
 from wtforms import ValidationError
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
     submit = SubmitField('Submit')
 
 class NewBlog(FlaskForm):
-    title = StringField('Blog title',validators=[Required()])
-    blog_content = TextAreaField('Blog to post',validators=[Required()])
-    author = StringField('Author',validators=[Required()])
+    title = StringField('Blog title',validators=[InputRequired()])
+    blog_content = TextAreaField('Blog to post',validators=[InputRequired()])
+    author = StringField('Author',validators=[InputRequired()])
     submit = SubmitField('submit')
 
 class MyComment(FlaskForm):
-    description = TextAreaField('Tell us what you think',validators=[Required()])
+    description = TextAreaField('Tell us what you think',validators=[InputRequired()])
     submit = SubmitField('submit')
 
 class UpdateBlog(FlaskForm):
-    updates = TextAreaField('update your blog',validators=[Required()])
+    updates = TextAreaField('update your blog',validators=[InputRequired()])
     submit = SubmitField('submit')
 
 class SubscribeForm(FlaskForm):
-    email = StringField('Your email address',validators=[Required(),Email()])
+    email = StringField('Your email address',validators=[InputRequired(),Email()])
     submit = SubmitField('submit')
