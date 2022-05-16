c51441d374a28e92d4aaf971d6ddb58ae3e66353
blancc-page <blancc.666@gmail.com>
2022-05-16 15:20:28 +0300

    feat[config,manage]: build config.py and manage.py

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import config
+
+class Config:
+    SECRET_KEY = 'SECRET_KEY'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:dclxvi@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:dclxvi@localhost/blog'
+    DEBUG = True
+
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
